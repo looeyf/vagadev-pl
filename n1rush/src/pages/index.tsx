2d548e4d81366cf76c710f75e35a7159fe5cda7d
Luis Fernando de Oliveira <luis10.pf@gmail.com>
2021-06-04 22:12:53 -0300

    novo carousel e controls

--- n1rush/src/pages/index.tsx
+++ n1rush/src/pages/index.tsx
@@ -1,64 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { CarouselProvider, Slider, Slide, Image, ButtonBack, ButtonNext, DotGroup, CarouselContext  } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import {HiOutlineChevronLeft, HiOutlineChevronRight} from 'react-icons/hi';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
-  const statusFormatter = (current, total) => {
-    return `${current} / ${total}`
+  const carouselSettings = {
+    visibleSlides: 1,
+    totalSlides: 2,
+    naturalSlideWidth: 1920,
+    naturalSlideHeight: 840,
+    showAsSelectedForCurrentSlideOnly: true,
   };
 
-  const renderArrowPrev = (clickHandler, hasPrev, label) => {
-    return (
-      <HiOutlineChevronLeft onClick={clickHandler} className={styles.previousArrow} />
-    )
-  };
-
-  const renderArrowNext = (clickHandler, hasPrev, label) => {
-    return (
-      <HiOutlineChevronRight onClick={clickHandler} className={styles.nextArrow} />
-    )
-  };
+  const currentSliderIndex = () => {
+    const carouselContext = useContext(CarouselContext);
+    const [currentSlide, setCurrentSlide] = useState(carouselContext.state.currentSlide);
+    useEffect(() => {
+      function onChange() {
+        setCurrentSlide(carouselContext.state.currentSlide);
+      }
+      carouselContext.subscribe(onChange);
+      return () => carouselContext.unsubscribe(onChange);
+    }, [carouselContext]);
+    return currentSlide;
+  }
 
   const [currentCarouselGame, setCurrentCarouselGame] = useState('Mortal Kombat'); // QUando chamar a API ja setar o valor com o primeiro item
-  const onChange = (index, {props}) => {
-    const game = props.children.props.alt;
-    setCurrentCarouselGame(game);
-  };
-
-  const carouselSettings = {
-    showThumbs: false,
-    showIndicators: false,
-    infiniteLoop: true,
-    statusFormatter,
-    showArrows: false,
-    renderArrowPrev,
-    renderArrowNext,
-    onChange,
-  };
 
   return (
-    <div className={styles.carouselWrapper}>
-      
-      {/* transportar o carousel para um componente, menos o controller pq será diferente na outra section */}
-      <Carousel className={styles.carousel} {...carouselSettings}>  
-        <div>
-            <img src="/mortal-kombat.jfif" alt="Mortal Kombat" />
-        </div>
-        <div>
-            <img src="/rdr2.jfif" alt="Red Dead Redemption 2" />
-        </div>
-      </Carousel>
+    <CarouselProvider {...carouselSettings}>
+      <div className={styles.carouselWrapper}>
+        <Slider>
+          <Slide index={0}>
+            <Image hasMasterSpinner src="./mortal-kombat.jfif" alt="Mortal Kombat" />
+          </Slide>
+          <Slide index={1}>
+            <Image hasMasterSpinner src="./rdr2.jfif" alt="Red Dead Redemption 2" />
+          </Slide>
+        </Slider>
 
-      {/* Preciso ajustar as setas e o contador, está ruim, testar outro slider que tenha custom buttons */}
-      <div className={styles.carouselController}>
-        <p>{currentCarouselGame}</p> 
-        <span />
+        <div className={styles.carouselControllerWrapper}>
+          <div className={styles.title}>
+            <p>{currentCarouselGame}</p> 
+            <span />
+          </div>
+
+          <div className={styles.controls}>
+            <p>{currentSliderIndex} / 2</p>
+            <div>
+              <ButtonBack>
+                <HiOutlineChevronLeft />
+              </ButtonBack>
+              <ButtonNext>
+                <HiOutlineChevronRight />
+              </ButtonNext>
+            </div>
+
+          </div>
+        </div>
       </div>
-    </div>
-
-  )
+    </CarouselProvider>
+)
 }
