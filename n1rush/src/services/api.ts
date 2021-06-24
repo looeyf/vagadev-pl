1c543982720e795ce67e1f97ff104685b16d530f
Luis Fernando de Oliveira <luis10.pf@gmail.com>
2021-06-23 21:10:33 -0300

    variaveis de ambiente

--- n1rush/src/services/api.ts
+++ n1rush/src/services/api.ts
@@ -7,8 +7,8 @@ export const myApi = axios.create({
 export const api = axios.create({
   baseURL: 'https://free-to-play-games-database.p.rapidapi.com/api/',
   headers: {
-    "x-rapidapi-key": "23ad155296msh2dc944151ff3e8dp15e1bfjsn8fc9bbbd1c30",
-	  "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-	  "useQueryString": true
+    "x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
+	  "x-rapidapi-host": process.env.X_RAPIDAPI_HOST,
+	  "useQueryString": process.env.USE_QUERY_STRING
   }
 })
